aa2d88db21880ddf966ace98897920884b2cfc3e
Shounak Pandit <68001165+shounak-create@users.noreply.github.com>
2026-09-15 20:13:31 +0530

    3 problems sovled

---
+++ basics.ts
@@ -0,0 +1,16 @@
+//Declare variables for your name, age, and whether you're a student, with explicit type annotations.
+
+let name: string = "ABC";
+let age: number = 22;
+let isStudent:boolean = true
+
+// Create an array of your top 5 favorite technologies (strings).
+let movies: string[] = ["interstellar","martian"]
+
+// Create a tuple representing a student record: [name, rollNumber, isPassed] where name is string, rollNumber is number, isPassed is boolean.
+let StudentRecord: [string,number,boolean]= ["ABC",22,true]
+
+// Write a function square(n: number): number that returns the square of a number.
+function square(n:number){
+    return n*n
+}
