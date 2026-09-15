5c7902196e416f66c86fa4d2af4abc8e261d0a06
Shounak Pandit <68001165+shounak-create@users.noreply.github.com>
2026-09-15 20:24:20 +0530

    new files by running current file

--- basics.js
+++ basics.js
@@ -1,32 +1,26 @@
+"use strict";
 //Declare variables for your name, age, and whether you're a student, with explicit type annotations.
-
-let name: string = "ABC";
-let age: number = 22;
-let isStudent:boolean = true
-
-console.log(name,age,isStudent);
-
-
+let name = "ABC";
+let age = 22;
+let isStudent = true;
+console.log(name, age, isStudent);
 // Create an array of your top 5 favorite technologies (strings).
-let movies: string[] = ["interstellar","martian"]
-
-for(let i:number = 0;i<movies.length;i++){
+let movies = ["interstellar", "martian"];
+for (let i = 0; i < movies.length; i++) {
     console.log(movies[i]);
 }
-
 // Create a tuple representing a student record: [name, rollNumber, isPassed] where name is string, rollNumber is number, isPassed is boolean.
-let StudentRecord: [string,number,boolean]= ["ABC",22,true]
-for(let i:number = 0;i<StudentRecord.length;i++){
+let StudentRecord = ["ABC", 22, true];
+for (let i = 0; i < StudentRecord.length; i++) {
     console.log(StudentRecord[i]);
 }
-
 // Write a function square(n: number): number that returns the square of a number.
-function square(n:number): number{
-    return n*n
+function square(n) {
+    return n * n;
 }
 square(5);
 //Write a function logMessage(msg: string): void that just prints the message (no return value).
-function logMessage(msg:string):void{
+function logMessage(msg) {
     console.log(msg);
 }
-logMessage("No return in VOID...")
+logMessage("No return in VOID...");
