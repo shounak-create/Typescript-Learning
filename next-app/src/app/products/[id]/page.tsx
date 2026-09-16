768766b7a3b8d87a1418991dfa6a53d93e99bf54
Shounak Pandit <68001165+shounak-create@users.noreply.github.com>
2026-09-16 13:58:04 +0530

    stage 1 is completed

--- next-app/src/app/products/[id]/page.tsx
+++ next-app/src/app/products/[id]/page.tsx
@@ -1,7 +1,18 @@
-export default function Blog({ children }: { children: React.ReactNode }){
-    return(
-        <div>
-      {children}
-        </div>
-    )
+// app/products/[id]/page.tsx
+import { notFound } from 'next/navigation';
+
+const validIds = ['1', '2', '3'];
+
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  if (!validIds.includes(id)) {
+    notFound();
+  }
+
+  return <h1>Product ID: {id}</h1>;
 }
