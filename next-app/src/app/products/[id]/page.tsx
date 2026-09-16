955332c526a1023920714c3f5196c54997e9fe25
Shounak Pandit <68001165+shounak-create@users.noreply.github.com>
2026-09-16 13:48:07 +0530

    fixes

--- next-app/src/app/products/[id]/page.tsx
+++ next-app/src/app/products/[id]/page.tsx
@@ -1,9 +1,6 @@
-import { setTimeout } from "node:timers/promises";
-
 export default function Blog({ children }: { children: React.ReactNode }){
     return(
         <div>
-            {setTimeout(()=><div>hello</div>,[1000])}
       {children}
         </div>
     )
