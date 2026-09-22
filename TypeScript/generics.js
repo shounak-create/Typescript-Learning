e20e91a94c6acbe8cff4b6ea8ae0159bc0314a9f
Shounak Pandit <68001165+shounak-create@users.noreply.github.com>
2026-09-22 14:48:46 +0530

    excercise 2 complete

--- TypeScript/generics.js
+++ TypeScript/generics.js
@@ -5,7 +5,8 @@ function getLastElement(arr) {
     return arr[len - 1];
 }
 console.log(getLastElement([2, 4, 1, 5]));
-// Create a generic interface Pair<T, U> with first: T and second: U. Create an object using it with a string and a boolean.
+let pair = { first: "age", second: true };
+console.log(pair);
 // Create a generic class Stack<T> with methods push(item: T): void, pop(): T | undefined, and peek(): T | undefined. Use it with numbers.
 // Write a generic function logProperty<T extends { name: string }>(item: T): void that logs item.name. Test it with an object that has extra properties too.
 // Create a generic function wrapInArray<T>(value: T): T[] that returns the value wrapped in an array. Test with a string and a number.
